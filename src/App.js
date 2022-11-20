23261feb94b098f99518254ce29160deb85eb0e9
AnastasiiaKmit <anastasiiakmit99@gmail.com>
2022-11-20 19:06:03 +0000

    set props and API call

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./app.css";
-import Temperature from "./Temperature";
+import Weather from "./Weather";
 
 export default function App() {
-  let [city, setCity] = useState("");
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log({ city });
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
   return (
     <div className="App">
-      <div className="mb-3">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                className="form-control"
-                autoComplete="off"
-                placeholder="Type a city..."
-                onChange={updateCity}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                className="btn btn-primary w-100"
-                value="Search"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-
-      <Temperature city={city} />
+      <Weather defaultCity="Lviv" />
     </div>
   );
 }
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,15 +1,43 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
 
-import "./temperature.css";
+import "./weatherDetails.css";
 
-export default function Temperature() {
-  function showCity(props) {
-    return;
-    <h1>{props.city}</h1>;
-  }
+export default function WeatherDetails(props) {
+  return (
+    <div className="weatherDetails">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li>{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-image">{props.data.icon}</div>
+          <div className="float-left">
+            <strong>{props.data.temperature}</strong>
+
+            <span className="units">
+              <a href="/">°C</a> |<a href="/">°F</a>
+            </span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*export default function Temperature() {
   return (
     <div className="Temperature">
-      {showCity}
       <ul>
         <li>Last updated: Tuesday 10:00</li>
         <li>Cloudy</li>
@@ -29,16 +57,11 @@ export default function Temperature() {
             </span>
           </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: 80%</li>
-            <li>Wind: 10 km/h</li>
-          </ul>
-        </div>
+        <div className="col-6"></div>
       </div>
     </div>
   );
-}
+}*/
 
 /*import React, { useState } from "react";
 import axios from "axios";
@@ -93,4 +116,9 @@ export default function Temperature(props) {
     return <p>Loading...</p>;
   }
 }
-//lat={lat}&lon={lon}*/
+//lat={lat}&lon={lon}
+<img
+              src='http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.data.icon}.png'
+              alt="weather icon"
+            />
+*/
