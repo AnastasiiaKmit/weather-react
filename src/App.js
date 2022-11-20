7982e4344e9a052cd0d54978eb76058777fdd602
AnastasiiaKmit <anastasiiakmit99@gmail.com>
2022-11-20 22:20:36 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Lviv" />
+      <div className="container">
+        <Weather defaultCity="Lviv" />
+        <footer>
+          <div>
+            This project was coded by Anastasiia Kmit and is{" "}
+            <a href="/">open-sourced on Github</a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
